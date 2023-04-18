a15e584448c1402968f92440523b2d05021dfbda
DashaVed <dasha.ved93@gmail.com>
2023-04-18 11:14:32 +0300

    + registration api view

---
+++ backend/src/api/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from web.models import User
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'first_name', 'last_name', 'city']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def save(self):
+        user = User.objects.create_user(
+            email=self.validated_data['email'],
+            first_name=self.validated_data['first_name'],
+            last_name=self.validated_data['last_name'],
+            city=self.validated_data['city'],
+        )
+        return user
+
+
+class TokenResponseSerializer(serializers.Serializer):
+    token = serializers.CharField()
--- backend/src/api/views.py
+++ backend/src/api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.db.models import Prefetch
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status, mixins
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes, action
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
-# Create your views here.
+from api.serializers import RegistrationSerializer, TokenResponseSerializer
+
+
+@api_view(['POST'])
+@permission_classes([])
+def registration_view(request):
+    serializer = RegistrationSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response({'email': user.email})
+    else:
+        return Response({'error': 'dont register'})
+
+
+
