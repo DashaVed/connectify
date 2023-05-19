2d05edd725c3d466e3bd70e8bde0fcc65d34b0ed
DashaVed <dasha.ved93@gmail.com>
2023-05-20 00:21:56 +0300

    + brand redirect to /home, when user is auth

--- frontend/src/components/HeaderOnlyWithTitle.vue
+++ frontend/src/components/HeaderOnlyWithTitle.vue
@@ -1,19 +1,28 @@
 <template>
-    <header>
-        <w-toolbar shadow>
-            <router-link :to="{name: 'main'}">
-                <div class="title2 text-bold blue-dark6">Connectify</div>
-            </router-link>
-            <div class="spacer"></div>
-            <slot></slot>
-        </w-toolbar>
-    </header>
+  <header>
+    <w-toolbar shadow>
+      <router-link :to="{name: 'main'}" v-if="!isAuth">
+        <div class="title2 text-bold blue-dark6">Connectify</div>
+      </router-link>
+      <router-link :to="{name: 'home'}" v-if="isAuth">
+        <div class="title2 text-bold blue-dark6">Connectify</div>
+      </router-link>
+      <div class="spacer"></div>
+      <slot></slot>
+    </w-toolbar>
+  </header>
 </template>
 
 <script>
 
+import {useAuthStore} from "@/stores/auth";
+import {mapState} from "pinia";
+
 export default {
-    name: "HeaderOnlyWithTitle",
+  name: "HeaderOnlyWithTitle",
+  computed: {
+    ...mapState(useAuthStore, ["isAuth"]),
+  }
 }
 </script>
 
